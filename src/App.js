05c5a1cf1d9686d57809b3c5766e825b043651ae
Franklin Oni-Ossai <67171927+guitaiconic@users.noreply.github.com>
2023-10-19 12:28:25 +0100

    dojo-blog

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import  {BrowserRouter as Router, Route, Switch}  from 'react-router-dom'
 
 
 function App() {
@@ -7,12 +8,18 @@ function App() {
 
 
   return (
-    <div className="App">
-    <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+      <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,16 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
 
 
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const[error, setError] = useState(null);
-
-
-
+  const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
   
-  useEffect(()=>{
-    fetch( 'http://localhost:8000/blogs')
-      .then(res =>{
-        
-        if(!res.ok){
-          throw Error('Could not fetch data for that resource')
-        }
-        return res.json()
-      })
-      .then(data => {
-        setBlogs(data)
-        setIsPending(false);
-        setError(null)
-      })
-      .catch (err => {
-        setIsPending(false);
-        setError(err.message);
-      })
-  },[]);
-
-
     return ( 
        <div className="home">
-       {error && <div>{error}</div>}
+        {error && <div>{error}</div>}
         { isPending && <div>Loading...</div>}
         {blogs && <BlogList blogs= {blogs} title= "All Blogs!" />} 
        </div> 
